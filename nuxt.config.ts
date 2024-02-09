ee03fdef9db75f38ec7b4b977aa1218f244fc8fd
Vladislav Torgovtsev <vlad4990@gmail.com>
2024-02-10 02:36:02 +0300

    update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    baseURL: '/orion-lp/',
+    buildAssetsDir: 'assets',
+  },
   css: ['~/assets/scss/style.scss'],
   vite: {
     css: {
